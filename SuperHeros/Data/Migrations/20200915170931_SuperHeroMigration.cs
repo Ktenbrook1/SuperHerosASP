3f44d19659c7be831036c5d47add15a67716e50b
ktenbrook1 <katelynntenbrook@gmail.com>
2020-09-15 15:17:44 -0500

    init

---
+++ SuperHeros/Data/Migrations/20200915170931_SuperHeroMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHeros.Data.Migrations
+{
+    public partial class SuperHeroMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeros",
+                columns: table => new
+                {
+                    name = table.Column<string>(nullable: false),
+                    alterEgo = table.Column<string>(nullable: true),
+                    primaryAbility = table.Column<string>(nullable: true),
+                    secondaryAbility = table.Column<string>(nullable: true),
+                    catchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeros", x => x.name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeros");
+        }
+    }
+}
---
+++ SuperHeros/Models/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeros.Models
+{
+    public class SuperHero
+    {
+        //porperties for the object
+        //will represent table in database
+        [Key]
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }
+        public string catchPhrase { get; set; }
+    }
+}
